1507e05c290ece586d2754e65c4cc2d9e86f6fd1
Dak <67485000+Dmelcher16@users.noreply.github.com>
2020-10-08 12:13:01 -0700

    still working on deployment

--- src/index.js
+++ src/index.js
@@ -1,11 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import SearchName from "./components/search"
+import Table from "./components/Table"
+import employees from "./employees.json"
 
-function Index(){
-    ReactDOM.render(<App />, document.getElementById('root'));
+class Index extends Component {
+  state = {
+    employees,
+    search: ""
+  };
 
+  render(){
+    return(
+      <div>
+        <table className = "table table-striped">
+        <Header />
+        <SearchName />
+        <thread>
+        <tr>
+        <th scope="col">Image</th>
+        <th scope="col">Name</th>
+        <th scope="col">Phone</th>
+        <th scope="col">Email</th>
+        <th scope="col">DOB</th>
+        </tr>
+    </thread>
+        {this.state.employees.map(employee => (
+          <Table 
+          id={employee.id}
+          img={employee.image}
+          name={employee.name}
+          phone={employee.phone}
+          email={employee.email}
+          dob={employee.dob}
+          />
+        ))}
+        </table>
+      </div>
+    )
+  }
 }
 
-export default Index
+// function App() {
+//   return (
+//     <div>
+//     <Header />
+//     <SearchName />
+//     <Table />
+//     </div>
+//   )
+
+// }
+
+export default Index;
